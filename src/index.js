d11713f7574a56a2aa5f13b5c74facf1f677c2ab
Kui Yang <c6swift@gmail.com>
2017-08-28 10:15:59 +0800

    兼容 webpack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import './css/style.css';
 import $ from 'jquery';
 import Popper from 'popper.js';
 import 'bootstrap';
-import imgs from './img/12.26.jpeg';
-console.log(imgs);
+$('[data-toggle="tooltip"]').tooltip();
+let isCollapsed = false;
+$('#allcollapse').click(function(e) {
+  e.preventDefault();
+  let target = $(this);
+  if (isCollapsed) {
+    $('div.collapse').collapse('hide');
+    target.html('展开全部');
+  } else {
+    $('div.collapse').collapse('show');
+    target.html('折叠全部');
+  }
+  isCollapsed = !isCollapsed;
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,88 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
-// const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+const PUBLIC_PATH = 'fishone.xyz';
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: PUBLIC_PATH
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ['style-loader', 'css-loader']
       },
       {
         test: /\.(png|svg|jpe?g|gif|m4r)$/,
-        use: ['file-loader'],
+        use: ['file-loader']
       },
       {
         test: /\.(scss|sass)$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: ['style-loader', 'css-loader', 'sass-loader']
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ['file-loader'],
+        use: ['file-loader']
       },
-    ],
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader'
+          }
+        ]
+      }
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html'
     }),
+    new HtmlWebpackPlugin({
+      template: './src/reason.html',
+      filename: 'reason.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/secret.html',
+      filename: 'secret.html'
+    }),
     new webpack.HotModuleReplacementPlugin(),
-    // new webpack.optimize.CommonsChunkPlugin({ name: 'common' }),
+    new webpack.optimize.CommonsChunkPlugin({ name: 'common' }),
     new webpack.ProvidePlugin({
       $: 'jquery',
       jQuery: 'jquery',
       Popper: ['popper.js', 'default']
     }),
-    // new CleanWebpackPlugin(['dist']),
+    new CleanWebpackPlugin(['dist']),
+    new SWPrecacheWebpackPlugin({
+      cacheId: 'my-project-name',
+      dontCacheBustUrlsMatching: /\.\w{8}\./,
+      filename: 'service-worker.js',
+      minify: true,
+      navigateFallback: PUBLIC_PATH + 'index.html',
+      staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/],
+      staticFileGlobs: [
+        'src/img/**.*',
+        'src/fonts/*',
+        'src/css/style.css',
+      ],
+      mergeStaticsConfig: true
+    })
   ],
   resolve: {
-    extensions: ['.json', '.js', '.jsx', '.css', '.html', 'ts', 'tsx'],
+    extensions: ['.json', '.js', '.jsx', '.css', '.html', 'ts', 'tsx']
   },
   devtool: 'source-map',
   devServer: {
     contentBase: path.resolve(__dirname, 'dist'),
     hot: true,
-    inline: true
-  },
+    inline: true,
+    open: true
+  }
 };
