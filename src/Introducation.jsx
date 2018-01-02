981456e08e27af1a600364906b6c9ed7d1f7fbe6
YangKui <c6swift@gmail.com>
2018-01-02 11:42:04 +0800

    抽出 zooming

--- src/Introducation.jsx
+++ src/Introducation.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Hero from './components/Hero'
 import Card from './components/Card'
 
-import Zooming from 'zooming';
+import Zooming from './zooming.js';
 
 const zooming = new Zooming({
   bgColor: '#000',
