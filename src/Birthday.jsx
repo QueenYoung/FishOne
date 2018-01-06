23328c872f8589d94c91f38fd10d10e7a35f5398
YangKui <c6swift@gmail.com>
2018-01-06 23:59:07 +0800

    使用 ReadStream 读取音乐, 不考虑兼容性问题…

--- src/Birthday.jsx
+++ src/Birthday.jsx
@@ -4,29 +4,55 @@ import 'bulma-pageloader/bulma-pageloader.min.css';
 import 'swiper/dist/css/swiper.min.css';
 import Swiper from 'swiper/dist/js/swiper.min';
 import SwiperContainer from './components/speceial/SwiperItem';
+import bgm from './bgm.mp3';
 
 class Birthday extends Component {
-  state = { 
+  state = {
     pageLoading: true,
     words: [],
     canRemoveModal: false
-  }
+  };
 
+  fetchAudioStream = () => {
+    fetch(bgm)
+      .then(res => {
+        const reader = res.body.getReader();
+        return new ReadableStream({
+          start(controller) {
+            return (function pump() {
+              return reader.read().then(({ done, value }) => {
+                if (done) {
+                  controller.close();
+                  return;
+                }
 
+                controller.enqueue(value);
+                return pump();
+              });
+            })();
+          }
+        });
+      })
+      .then(stream => new Response(stream))
+      .then(res => res.blob())
+      .then(blob => URL.createObjectURL(blob))
+      .then(src => this.audio.src = src)
+      .catch(console.log);
+  };
   toggleModal = () => {
     if (this.state.canRemoveModal) {
       document.querySelector('.navbar').style.opacity = 1;
       unmountComponentAtNode(document.querySelector('#happy'));
     }
-  }
+  };
 
   async componentDidMount() {
-    const [module, bgm] = await Promise.all([
+    this.fetchAudioStream();
+    const [module] = await Promise.all([
       import('./components/speceial/words.js'),
-      import('./bgm.mp3')
+      new Promise(resolve => setTimeout(resolve, 3000))
     ]);
     this.setState(({ pageLoading }) => ({ pageLoading: false }));
-    this.audio.src = bgm;
     this.setState({
       words: module.default,
       active: Array(module.default.length).fill(false)
@@ -40,49 +66,52 @@ class Birthday extends Component {
         el: '.swiper-pagination',
         dynamicBullets: true,
         click: true
-      },
+      }
     });
 
     let pos = 0;
-    mySwiper.on('scroll', (event) => {
+    mySwiper.on('scroll', event => {
       this.setState(({ active }) => ({
-        active: active.map((status, i) =>
-          i === pos ? true : status
-        )
+        active: active.map((status, i) => (i === pos ? true : status))
       }));
       // 用来检测是否滑动到最后一页了. 似乎 swipper 有 bug, 所以 || 的后面一分支 hack 了这个 bug
-      if (document.querySelector('.swiper-slide.swiper-slide-active:last-child') || 
-          !document.querySelector('.swiper-slide-next')) {
+      if (
+        document.querySelector(
+          '.swiper-slide.swiper-slide-active:last-child'
+        ) ||
+        !document.querySelector('.swiper-slide-next')
+      ) {
         this.audio.pause();
-        document.querySelector('.icon.has-text-danger')
+        document
+          .querySelector('.icon.has-text-danger')
           .classList.remove('is-active');
         this.setState({ canRemoveModal: true });
       }
       pos += 1;
     });
   }
+
   render() {
     const { pageLoading, words, active } = this.state;
     return (
       <Fragment>
-        <div className={`pageloader is-danger ${pageLoading ? 'is-active' : ''}`}
+        <div
+          className={`pageloader is-danger ${pageLoading ? 'is-active' : ''}`}
           style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}>
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}>
           <span className="title">精彩内容正在准备! 玩命加载中ヾ(=･ω･=)o </span>
         </div>
 
         <div className={`modal is-active`}>
-          <div className="modal-background" onClick={this.toggleModal}></div>
+          <div className="modal-background" onClick={this.toggleModal} />
           <div className="modal-content">
-            <SwiperContainer
-              words={words}
-              active={active} />
+            <SwiperContainer words={words} active={active} />
           </div>
         </div>
-        <audio src="" autoPlay loop ref={node => this.audio = node}/>
+        <audio src="" autoPlay loop ref={node => (this.audio = node)} />
       </Fragment>
     );
   }
